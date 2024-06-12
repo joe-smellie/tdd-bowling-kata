802e9b4e1b66f2911958e83324e399eac2f9ca73
Joe Smellie <joesmellie1@gmail.com>
2024-06-12 20:50:53 +1200

    rejig

--- game.js
+++ game.js
@@ -1,38 +1,33 @@
 
 //helps to keep code simple
 
-/*export function scoreFrame(arr) {
-    return arr[0] + arr[1]
-}*/
 
+
+const frames = [
+    [6, 1], [4, 0], [6, 4], [2, 7], [3, 5], [5, 0], [5, 5], [0, 0], [1, 6], [7, 2]
+  ]
 // function for scoring bowling frames
-export function scoreFrames(frames){
+export function frameScore(frames){
 
-    //create a variable to store totalscore
-    let totalScore = 0
-    
-    //calculate score from each frame
-    frames.forEach((frame, i) => {
-      let frameScore = frame[0] + frame[1] 
-    // if a spare is bowled calculate score
-    if (isGutterball(frame))
-        frameScore = frame[0] + frame[1]
-    
-    if (isSpare(frame)) {
-      frameScore = frame[0] + frame[1] + frames[i + 1][0]
-     }
-    totalScore += frameScore
-    })
-    
-    return totalScore
-    }
-    
-    function isSpare(frame) {
-      if (frame[0] === 10) return false
-      return frame[0] + frame[1] ===10
-    
-    }
+//create a variable to store totalscore
+let totalScore = 0
 
-    function isGutterball(frame) {
-        return frame[0] + frame[1] === 0
-    }
+//calculate score from each frame
+frames.forEach((frame, i) => {
+  let frameScore = frame[0] + frame[1] 
+// if a spare is bowled calculate score
+ if (isSpare(frame)) {
+  frameScore = frame[0] + frame[1] + frames[i + 1][0]
+ }
+totalScore += frameScore
+})
+
+console.log(totalScore)
+return totalScore
+}
+
+function isSpare(frame) {
+  if (frame[0] === 10) return false
+  return frame[0] + frame[1] ===10
+
+}
--- game.test.js
+++ game.test.js
@@ -2,60 +2,97 @@
 // first thing I want it to do
 //start small - bottom up
 
+import * as score from "./game.js";
+import { describe, test, expect } from "vitest"; // inbuilt methods from vitest
 
-import { scoreFrame } from './game.js'
-import { describe, test, expect } from 'vitest' // inbuilt methods from vitest
+test("tests setup working", () => {
+  expect(true).toBeTruthy();
+});
 
-describe('scoreFrame', () => {
-    test('should score a gutterball', () => {
-    //Arrange
-    const frame = [0, 0]
-    const expectedOutput = 0 
+//describe("scoreFrame", () => {
+test("should score a gutterball", () => {
+  //Arrange
+  const frames = [[0, 0]];
+  const expectedOutput = 0;
 
-    //Act
-    const actualOutput = scoreFrame(frame)
+  //Act
+  const actualOutput = score.frameScore(frames);
 
-    //Assert - what you want the function to do
-    expect(actualOutput).toBe(expectedOutput)
-    })
+  //Assert - what you want the function to do
+  expect(actualOutput).toBe(expectedOutput);
+});
 
-    test('should score a simple frame', () => {
-    //Arrange
-   /* const frame =[6, 2]
-    const expectedOutput = 8
+test("should score a simple frame", () => {
+  //Arrange
+  const frames = [[6, 2]];
+  const expectedOutput = 8;
 
-    //Act
-    const actualOutput = scoreFrame(frame)
+  //Act
+  const actualOutput = score.frameScore(frames);
 
-    //Assert
-    expect(actualOutput).toBe(expectedOutput)*/
-    expect(scoreFrame([3, 2])).toBe(5)
-    expect(scoreFrame([7, 1])).toBe(8)
-    expect(scoreFrame([1, 3])).toBe(4)
-    })
+  //Assert
+  expect(actualOutput).toBe(expectedOutput);
+});
 
-    test('should score a spare'), () => {
-        //Arrange
-        const frame = [5, 5]
+test("should score a spare", () => {
+  //Arrange
+  const frames = [
+    [9, 1],
+    [6, 3],
+  ];
+  const expectedOutput = 25;
 
-        //Act
-        const actualOutput = scoreFrame(frame)
+  //Act
+  const actualOutput = score.frameScore(frames);
 
-        //Assert
-        expect(actualOutput).toBe(expectedOutput)
-    }
-    
-    test('should score a strike'), () => {
-        const frame = [10, 0]
+  //Assert
+  expect(actualOutput).toBe(expectedOutput);
+});
 
-        const actualOutput = scoreFrame(frame)
+//Only went up to spare in bowling kata
+// test("should score a strike", () => {
+//   const frames = [[10, 0],[5, 2]];
+//   const expectedOutput =
 
-        expect(actualOutput).toBe(expectedOutput)
-    }
-})
+//   const actualOutput = score.frameScore(frames);
 
-describe('scoreGame', () => {
-    test.todo('should score a simple game')
-    test.todo('should score a spare game')
-})
+//   expect(actualOutput).toBe(expectedOutput);
+// });
 
+//describe("scoreGame", () => {
+test("should score a simple game", () => {
+  const frames = [
+    [2, 0],
+    [4, 2],
+    [6, 0],
+    [2, 4],
+    [1, 5],
+    [7, 0],
+    [5, 2],
+    [7, 0],
+    [2, 6],
+    [8, 1],
+  ];
+  const expectedOutput = 64;
+  const actualOutput = score.frameScore(frames);
+
+  expect(actualOutput).toBe(expectedOutput);
+});
+test("should score a spare game", () => {
+  const frames = [
+    [6, 1],
+    [4, 0],
+    [6, 4],
+    [2, 7],
+    [3, 5],
+    [5, 0],
+    [5, 5],
+    [0, 0],
+    [1, 6],
+    [7, 2],
+  ];
+  const expectedOutput = 71;
+  const actualOutput = score.frameScore(frames);
+
+  expect(actualOutput).toBe(expectedOutput);
+});
